aa8fa323759de077a6fb1d330948535b02ac8d9c
ravipavi143 <ravipavi143>
2022-08-02 12:24:41 +0530

    change files

--- file_provisioner.tf
+++ file_provisioner.tf
@@ -3,35 +3,4 @@ resource "aws_instance" "web" {
   ami = "ami-09de362f44ba0a166"
   instance_type = "t2.micro"
   key_name = "terraform"
-#  security_groups = ["sg-015fd4777c58fdc9c"]
-provisioner "file" {
-     source = "index.html"
-     destination = "/tmp/index.html"
-     on_failure = continue
-  }
-  connection {
-     type = "ssh"
-     host = self.public_ip
-     user = "ec2-user"
-     password = ""
-     private_key = file("terraform.pem")
-  }
 }
-/*
-data "aws_ami" "web" {
-  filter {
-    name   = "name"
-    values = ["cloudlinux-solo-8.6-x86_64.CPanel*"]
-  }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["679593333241"]
-  most_recent = true
-}
-*/
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAYZJW6EMCSUFI4LUZ"
-  secret_key = "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"
 }
